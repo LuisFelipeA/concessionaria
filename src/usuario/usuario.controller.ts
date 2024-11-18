371be9c0b8301320e28a48d215e5f614fe670855
LuisFelipeA <felipeadriani2@gmail.com>
2024-11-18 20:03:05 -0300

    criando usuario entity

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { UsuarioRepository } from "./usuario.repository";
 import { CriaUsuarioDTO } from "./dto/CriaUsuario.dto";
+import { UsuarioEntity } from "./usuario.entity";
+import { v4 as uuid } from "uuid"; // para usar uuid - npm install uuid e npm install -D @types/uuid
 
 @Controller('/usuarios')
 export class UsuarioController {
@@ -14,7 +16,15 @@ export class UsuarioController {
 
     @Post()
     async criaUsuario(@Body() dadosDoUsuario: CriaUsuarioDTO){
-        this.usuarioRepository.salvar(dadosDoUsuario);
-        return dadosDoUsuario
+        const usuarioEntity = new UsuarioEntity;
+        
+        usuarioEntity.email = dadosDoUsuario.email;
+        usuarioEntity.nome = dadosDoUsuario.nome;
+        usuarioEntity.senha = dadosDoUsuario.senha;
+        usuarioEntity.id = uuid();
+
+        this.usuarioRepository.salvar(usuarioEntity);
+
+        return { id: usuarioEntity.id, message: "Usuario Criado com Sucesso"}
     }
 }
