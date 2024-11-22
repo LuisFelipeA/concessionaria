184d42b41bac7046e0bcbcedb64b7d69d5605052
LuisFelipeA <felipeadriani2@gmail.com>
2024-11-21 21:21:28 -0300

    configurando TypeORM Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,23 @@ import { ModelosModule } from './modelos/modelos.module';
 import { MarcasModule } from './marcas/marcas.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { ProdutoModule } from './produto/produto.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ModelosModule, MarcasModule, UsuarioModule, ProdutoModule],
+  imports: [
+    ModelosModule, 
+    MarcasModule, 
+    UsuarioModule, 
+    ProdutoModule, 
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService]
+    })
+  ],
 })
 export class AppModule {}
---
+++ src/config/postgres.config.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
+
+@Injectable()
+export class PostgresConfigService implements TypeOrmOptionsFactory {
+    constructor(private configService: ConfigService){}
+
+    createTypeOrmOptions(): TypeOrmModuleOptions {
+        return {
+            type: 'postgres',
+            host: this.configService.get<string>('DB_HOST'),
+            port: this.configService.get<number>('DB_PORT'),
+            username: this.configService.get<string>('postgres'),
+            password: this.configService.get<string>('DB_PASSWORD'),
+            database: this.configService.get<string>('DB_NAME'),
+            entities: [],
+            synchronize: true
+        }
+    }
+}
+
+
+// npm install @nestjs/typeorm typeorm
+// npm install @nestjs/config
+
+// criar .env e configurar
+// ex:
+//  DB_HOST=127.0.0.1
+//  DB_PORT=5432
+//  DB_USER=postgres
+//  DB_PASSWORD=1234
+//  DB_NAME=loja
+
+// npm install pg --save
